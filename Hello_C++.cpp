13532cd6f5100af484b66410eac1aec7afc0c83d
matheusramires <80134011+matheusramires@users.noreply.github.com>
2021-03-21 15:46:58 -0300

    Add files via upload

---
+++ Hello_C++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+main (){
+	std::cout << "\aHello World";
+	std::cout << "\nSeja bem vindo ao C++";
+	std::cout <<"\nEste eh o meu primeiro programa em C++, criado no dia 20/03/2021.";
+	
+
+}
